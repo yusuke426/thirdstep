a3b433523419582d10daa715d649b452c1474562
mct-gosuzuki <j1618@matsue-ct.jp>
2019-10-09 11:00:46 +0900

    Added Hensachi, Score sort.

--- main.rb
+++ main.rb
@@ -3,11 +3,15 @@
 str = gets().chop()
 nums = str.split(' ').map{|i| i.to_i() }
 
-sum = 0
-nijo= 0
-nums.each {|i|
-  sum = sum + i
-  nijo = nijo + (i * i)
-}
+sum = nums.sum()
+variant = nums.map{|i| i * i}.sum() / nums.length
 avg = sum / nums.length
-print "Average:", avg, " SD:", Math.sqrt((nijo/nums.length) - (avg*avg)), " SUM:", sum, "\n"
+sd = Math.sqrt(variant - (avg*avg))
+hensachis = nums.map{|i| (i - avg) * 10 / sd + 50}
+
+print "Average:", avg, " SD:", sd, " SUM:", sum, "\n"
+
+data = nums.zip(hensachis)
+data.sort!{|a, b| a[0] <=> b[0]}.reverse!
+print "Score\t|Hensachi\n"
+data.each{|i| print i[0], "\t|", i[1], "\n" }
